525b384447fadc068b84cfd30510a3a0127bf6ba
mibasFerraz <lucasllff@gmail.com>
2024-06-22 18:12:53 -0300

    feat: page about

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="p-20 space-y-5">
+          <h1 className="text-6xl text-center pb-5">Sobre a InvestMinds</h1>
+          <p className="text-justify">
+            Claro! Aqui estão dois parágrafos que descrevem seu negócio: ---
+            **Pitch It** é uma plataforma web inovadora projetada para ajudar
+            estudantes e empreendedores de startups a criar pitches de vendas
+            impactantes. Utilizando inteligência artificial, a ferramenta guia o
+            usuário através de todas as etapas necessárias para elaborar uma
+            apresentação convincente, desde a estruturação do conteúdo até o
+            design visual. Com recursos intuitivos e fáceis de usar, o Pitch It
+            permite que os usuários desenvolvam pitches personalizados que
+            destacam os pontos fortes de seus projetos e capturam a atenção de
+            potenciais investidores.
+          </p>
+
+          <p>
+            Além de auxiliar na criação de pitches, o Pitch It oferece um
+            chatbot avançado alimentado por inteligência artificial para tirar
+            dúvidas em tempo real. Este assistente virtual está disponível para
+            responder a perguntas sobre estratégias de apresentação, sugestões
+            de melhorias e outras dúvidas relacionadas ao desenvolvimento de um
+            pitch eficaz. Com essa combinação de criação de conteúdo e suporte
+            inteligente, o Pitch It se posiciona como a solução definitiva para
+            quem busca maximizar o impacto de suas apresentações de vendas.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+export default About;
--- src/routes/signedin-routes.jsx
+++ src/routes/signedin-routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import SignedInLayout from "../layouts/signedin-layout";
+import About from "../pages/About/About";
 
 const signedInRouter = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const signedInRouter = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        index: "/about",
+        element: <About />,
       },
       {
         path: "/*",
